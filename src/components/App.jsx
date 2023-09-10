8b4eab0a3d75ba1085b5ed5bbb5cd10632a32a08
IgorMushk <igor.mushketik@gmail.com>
2023-09-10 17:49:35 +0300

    Add state in App + formSubmit - add in state words
    formSubmit - добавление объекта слова в массив слов

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
-import { Container } from "@mui/material";
-import WordsForm from "./WordsForm/WordsForm";
+import { Container } from '@mui/material';
+import WordsForm from './WordsForm/WordsForm';
 
-export const App = () => {
+import React, { Component } from 'react';
 
-  return (
-    <Container maxWidth="xl">
-      <WordsForm/>
-    </Container>
-  );
+export default class App extends Component {
+state = {
+  words: [],
 };
+
+// Метод, который при самбите формы добавляет объект слова в массив слов
+// Приходит объект слова с 3 ключами ukWord enWord id (генерим)
+// Нужно этот метод пропсом в компонент Form
+// Нужно передавать callback функцию, а не объект
+// callback будет возвращать объект
+formSubmit = (newWord) => {
+  this.setState(prevState =>({
+    words: [...prevState.words, newWord ]
+   }))
+}
+
+  render() {
+    return (
+      <Container maxWidth="xl">
+        <WordsForm onSubmit={this.formSubmit}/>
+      </Container>
+    );
+  }
+}
--- src/components/WordsForm/WordsForm.jsx
+++ src/components/WordsForm/WordsForm.jsx
@@ -1,35 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import css from  './WordForm.module.css'
+import css from './WordForm.module.css';
+import { nanoid } from 'nanoid';
 
 export default class WordsForm extends Component {
-    state = {
-        ukWord:'',
-        enWord:'',
-      }
-    
-    // Функция, которая  изменяет state когда вводим что-то в inpute  
-    handleChange = (evt) => {
-        // не выводится значение в консоль
-        //console.dir('evt.target', evt)
-        this.setState({[evt.target.name]: evt.target.value})
-      }
-    // Типа глубокая деструктуризация   
-    // handleChange = ({target:{name,value}}) => {
-    //     //console.log('evt', evt)
-    //     this.setState({[name]:value})
-    //   }
+  state = {
+    ukWord: '',
+    enWord: '',
+  };
 
-    render() {
+  // Функция, которая  изменяет state когда вводим что-то в inpute
+  handleChange = evt => {
+    // не выводится значение в консоль
+    //console.dir('evt.target', evt)
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+  // Типа глубокая деструктуризация
+  // handleChange = ({target:{name,value}}) => {
+  //     //console.log('evt', evt)
+  //     this.setState({[name]:value})
+  //   }
+
+  // ??? ...this.state , а не ...this.state.words
+  // т.к. это state WordFormn - новое введеное слово
+  handelSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit({ ...this.state, id: nanoid(5) });
+    this.setState({
+      ukWord: '',
+      enWord: '',
+    });
+  };
+
+  render() {
     return (
-      <div className={css.wordsFormWrapper}>
+      <div className={css.wordsFormWrapper} onSubmit={this.handelSubmit}>
         <form className={css.wordsForm}>
-        <TextField id="outlined-basic" label="Ukrainian" variant="outlined" value={this.state.ukWord} name="ukWord" onChange={this.handleChange}/>
-        <TextField id="outlined-basic" label="English" variant="outlined" value={this.state.enWord} name="enWord" onChange={this.handleChange}/>
-        <Button variant="contained">Add</Button>
+          <TextField
+            id="outlined-basic"
+            label="Ukrainian"
+            variant="outlined"
+            value={this.state.ukWord}
+            name="ukWord"
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="outlined-basic"
+            label="English"
+            variant="outlined"
+            value={this.state.enWord}
+            name="enWord"
+            onChange={this.handleChange}
+          />
+          <Button variant="contained" type="submit">
+            Add
+          </Button>
         </form>
       </div>
-    )
+    );
   }
 }
+
+// Т.к. это классовый компонет, то  до props можно достучатся через this.props
+// Но прежде чем стукатьсядо  props, нужно написать метод, который будет обрабатывать submin
